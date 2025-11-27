071b25e7b45adac9b92292c1846b4cf5c738676a
vargasnaut <nvargas20027@gmail.com>
2025-11-26 19:55:46 -0500

    "Limpieza del proyecto y .gitignore agregado"

---
+++ 01. fundamentos/modulo_math.py
@@ -0,0 +1,8 @@
+import math
+
+
+numero = int(input("ingresa un numero por favor: "))
+print(f"raiz de {numero} = {math.sqrt(numero)}")
+print(f"seno de {numero} = {math.sin(numero)}")
+print(f"exponencial del {numero} = {math.exp(numero)}")
+print(f"logaritmo del numero {numero} = {math.log({math.sqrt(numero)})}")
